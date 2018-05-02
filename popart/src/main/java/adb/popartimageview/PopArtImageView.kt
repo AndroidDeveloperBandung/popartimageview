04315cdf629a7cc2a6681f804e01626e28eef8b8
Esa Firman <esafirm21@gmail.com>
2018-05-02 17:51:59 +0700

    Complete

--- popart/src/main/java/adb/popartimageview/PopArtImageView.kt
+++ popart/src/main/java/adb/popartimageview/PopArtImageView.kt
@@ -17,16 +17,30 @@ class PopArtImageView @JvmOverloads constructor(
         set(value) {
             field = value
             imageBitmap = BitmapFactory.decodeResource(context.resources, value!!)
-            bitmapWidth = imageBitmap?.width?.toFloat() ?: 0F
-            bitmapHeight = imageBitmap?.height?.toFloat() ?: 0F
         }
 
     private var imageBitmap: Bitmap? = null
+        set(value) {
+            field = value
+
+            bitmapWidth = value?.width?.toFloat() ?: 0F
+            bitmapHeight = value?.height?.toFloat() ?: 0F
+
+            log("Bitmap width: $bitmapWidth")
+            log("Bitmap height: $bitmapHeight")
+        }
+
     private var bitmapWidth: Float = 0F
     private var bitmapHeight: Float = 0F
 
     private val paint by lazy { Paint() }
 
+    private val colors = mapOf(
+            "red" to Color.parseColor("#AAd50000"),
+            "yellow" to Color.parseColor("#AAffea00"),
+            "blue" to Color.parseColor("#AA00FFFF"),
+            "green" to Color.parseColor("#AA00c853")
+    )
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -40,31 +54,40 @@ class PopArtImageView @JvmOverloads constructor(
         super.onSizeChanged(w, h, oldw, oldh)
 
         if (imageBitmap != null) {
-            log("W $w")
-            log("H $h")
-            val destWidth = w / 4
-            val destHeight = h / 4
+            val destWidth = w / 2
+            val destHeight = h / 2
+
             imageBitmap = Bitmap.createScaledBitmap(imageBitmap, destWidth, destHeight, true)
 
-            log("Bitmap width: $bitmapWidth")
-            log("Bitmap height: $bitmapHeight")
-            invalidate()
+            log("destWidth $destWidth")
+            log("destHeight $destHeight")
         }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        if (image == null) return
+        if (imageBitmap == null) return
 
         log(measuredHeight)
         log(measuredWidth)
 
         canvas.drawBitmap(imageBitmap, 0F, 0F, paint)
         canvas.drawBitmap(imageBitmap, bitmapWidth, 0F, paint)
+        canvas.drawBitmap(imageBitmap, 0f, bitmapHeight, paint)
+        canvas.drawBitmap(imageBitmap, bitmapWidth, bitmapHeight, paint)
 
-        paint.color = Color.RED
-//        canvas.drawRect(0F, 0F, bitmapWidth, bitmapHeight, paint)
+        paint.color = colors["red"]!!
+        canvas.drawRect(0F, 0F, bitmapWidth, bitmapHeight, paint)
+
+        paint.color = colors["yellow"]!!
+        canvas.drawRect(0F, bitmapHeight, bitmapWidth, bitmapHeight * 2, paint)
+
+        paint.color = colors["blue"]!!
+        canvas.drawRect(bitmapWidth, 0F, bitmapWidth * 2, bitmapHeight, paint)
+
+        paint.color = colors["green"]!!
+        canvas.drawRect(bitmapWidth, bitmapHeight, bitmapWidth * 2, bitmapHeight * 2, paint)
     }
 
     private fun log(message: Any?) {
